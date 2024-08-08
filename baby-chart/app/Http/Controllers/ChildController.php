6c3ce4f2a5ba9d64824d38a4b8f783f9b7481c16
D-Binata <dbkaveesha@gmail.com>
2024-08-08 22:47:02 +0530

    save qr in database

--- baby-chart/app/Http/Controllers/ChildController.php
+++ baby-chart/app/Http/Controllers/ChildController.php
@@ -29,6 +29,7 @@ class ChildController extends Controller
             'weight' => 'nullable|numeric',
             'user_id' => 'required|exists:users,id',
             'midWifeId' => 'required|exists:mid_wives,id',
+            'qr_code' => 'nullable|string',
         ]);
 
         // Map the validated data to the correct column names in the database
@@ -45,6 +46,7 @@ class ChildController extends Controller
             'weight' => $validatedData['weight'],
             'user_id' => $validatedData['user_id'],
             'mid_wife_id' => $validatedData['midWifeId'],
+            'qr_code' => $validatedData['qr_code'],
         ];
 
         // Create a new child using the validated data
